7a241f4088a19101d8566882079491ba047b396a
Josh Mayer <jooshmayer@gmail.com>
2024-03-21 10:13:42 -0700

    title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,8 +9,8 @@ import { type Section } from '@/components/SectionProvider'
 
 export const metadata: Metadata = {
   title: {
-    template: '%s - Protocol API Reference',
-    default: 'Protocol API Reference',
+    template: '%s - Tollbit API Reference',
+    default: 'Tollbit API Reference',
   },
 }
 
